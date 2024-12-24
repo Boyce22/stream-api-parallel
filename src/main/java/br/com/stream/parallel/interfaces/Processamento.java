83abd61f674862f87f761150264cb737c5386387
Bernardo Alves <bernardojob2003@outlook.com>
2024-12-24 19:51:30 -0300

    feat(Processamento): criação do serviço responsável pelo processamento

---
+++ src/main/java/br/com/stream/parallel/interfaces/Processamento.java
@@ -0,0 +1,5 @@
+package br.com.stream.parallel.interfaces;
+
+public interface Processamento {
+    void processar();
+}
---
+++ src/main/java/br/com/stream/parallel/services/ProcessamentoSequencial.java
@@ -0,0 +1,10 @@
+package br.com.stream.parallel.services;
+
+import br.com.stream.parallel.interfaces.Processamento;
+
+public class ProcessamentoSequencial implements Processamento {
+    @Override
+    public void processar() {
+
+    }
+}
